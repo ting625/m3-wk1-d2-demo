b1c5fe43b8a6d6ead2e39d196eb6ff428e5aa664
Ruo Ting <ruoting@Ruo-MacBook-Air.attlocal.net>
2024-07-06 10:12:53 -0700

    add package.json exp_demo

---
+++ exp_demo3a.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/home', function(req, res){
+    res.send("Hello World!");
+});
+router.post ('/home', function(req, res){
+    res.send("You just call the post method at '/hello'!\n");
+});
+
+// export this router to use in exp-demo3b.js
+module.exports = router; 
---
+++ exp_demo3b.js
@@ -0,0 +1,8 @@
+var routedemo = require('./exp_demo3a.js');
+
+//both exp-demo3a.js and exp-demo3b.js are in same directory
+app.use('/routedemo', routedemo);
+
+app.listen(3000);
+
+
